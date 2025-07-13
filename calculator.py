dcd2fb4729e940f7eb49a100e7de73204313578f
ajtw7 <andrew.worsley91@gmail.com>
2025-07-13 15:48:50 -0400

    Square Root

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import math
+
 ## Class
 class Calculator:   
 
@@ -9,25 +11,40 @@ class Calculator:
     def add(self, x): 
         result = self.display_val + x
         self.display_val = result
-        return result
 
     def sub(self, x):
         result = self.display_val - x
         self.display_val = result
-        return result
 
     def multiply(self, x):
         result = self.display_val * x
         self.display_val = result
-        return result
     
     def divide(self, x):
         result = self.display_val / x
         self.display_val = result
-        return result
+    
+    def sqrt(self,):
+        # Is the current display in an error state already?
+        if self.display_val < 0:
+
+        # If yes → do nothing (or signal that the error must be cleared).
+            self.display_val = 'err'
+            return
+
+        result = math.sqrt(self.display_val)
+        self.display_val = result                
+
+        # Is the number on the display invalid for square root (e.g., negative)?
+        # If yes → update self.display_val = "Err"
     
     def clear(self):
         self.display_val = 0
+
+    
+
+    
+
     
 
 # add lots more methods to this calculator class.
